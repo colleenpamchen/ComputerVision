cee46382d4c321b88cb40b0218ae59062c8fd76b
Colleen Chen <colleen@dhcp-v099-050.mobile.uci.edu>
2017-04-26 11:48:16 -0700

    ex

--- detect_edge.m
+++ detect_edge.m
@@ -1,4 +1,13 @@
 %% detect_edge.m 
 
+img = imread('dilbert1.jpg');
+A = im2double(img);
+
+
+h = [1 zeros(1,20) 0.5 zeros(1,10)];
+x = [0 1:10 5.*ones(1,5) zeros(1,40)];
+y = conv(x,h);
+
+
 
 % conv2('same') 
