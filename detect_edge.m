226f2792875a2c7efe47cbee874ba34d436d3c6c
Colleen Pam Chen <colleenpamchen@gmail.com>
2017-04-26 18:39:07 -0700

    started on the edge detector

--- detect_edge.m
+++ detect_edge.m
@@ -1,13 +1,21 @@
 %% detect_edge.m 
 
+% img = imread('simpsons.jpg');
+% img = imread('zebra1.jpg');
+% img = imread('zebra2.jpg');
+
 img = imread('dilbert1.jpg');
+figure;imshow(img) 
 A = im2double(img);
+figure; imagesc(A)
+filt = fspecial('gaussian') ;
 
-
-h = [1 zeros(1,20) 0.5 zeros(1,10)];
-x = [0 1:10 5.*ones(1,5) zeros(1,40)];
-y = conv(x,h);
-
+% filt = fspecial('gaussian', hsize, sigma) ;
+% returns a rotationally symmetric Gaussian lowpass filter of size hsize 
+% with standard deviation sigma (positive). hsize can be a vector specifying 
+% the number of rows and columns in h, or it can be a scalar, in which case h 
+% is a square matrix. The default value for hsize is [3 3]; the default value 
+% for sigma is 0.5. Not recommended. Use imgaussfilt or imgaussfilt3 instead.
 
 
 % conv2('same') 
--- hwk2.m
+++ hwk2.m
@@ -1,1 +1,54 @@
-%% Homework 2 DFT 
+%% Homework 2 DFT #5 
+
+h = [zeros(1,49) 1 zeros(1,50)];
+figure; plot(h) 
+m= abs(fft(h));
+figure; plot(m); 
+
+
+
+x = [0 1:10 5.*ones(1,5) zeros(1,40)];
+% y = conv(x,h);                          
+p = unwrap(angle(y)); 
+
+%%
+fs = 10000;
+t = 0:1/fs:1.5;
+x1 = sawtooth(2*pi*50*t);
+x2 = square(2*pi*50*t);
+subplot(2,1,1)
+plot(t,x1)
+axis([0 0.2 -1.2 1.2])
+xlabel('Time (sec)')
+ylabel('Amplitude') 
+title('Sawtooth Periodic Wave')
+subplot(2,1,2)
+plot(t,x2)
+axis([0 0.2 -1.2 1.2])
+xlabel('Time (sec)')
+ylabel('Amplitude')
+title('Square Periodic Wave')
+
+%%
+t = [zeros(1,49) 1 zeros(1,50)];                     % Time vector
+x = sin(2*pi*15*t) + sin(2*pi*40*t);      % Signal
+y = fft(x);                               % Compute DFT of x
+m = abs(y);                               % Magnitude
+f = (0:length(y)-1)*100/length(y);        % Frequency vector
+
+figure;
+subplot(2,1,1)
+plot(f,m)
+title('Magnitude')
+ax = gca;
+ax.XTick = [15 40 60 85];
+
+subplot(2,1,2)
+plot(f,p*180/pi)
+title('Phase')
+ax = gca;
+ax.XTick = [15 40 60 85];
+
+
+
+
